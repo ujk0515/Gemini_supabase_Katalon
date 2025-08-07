548392f90921a3bf257f49170790b59249c46246
ujk0515 <114380428+ujk0515@users.noreply.github.com>
2025-08-07 11:23:43 +0900

    Update gemini-keywords.js

--- libs/gemini-keywords.js
+++ libs/gemini-keywords.js
@@ -8,8 +8,8 @@ class GeminiKeywordExtractor {
         this.apiKey = 'AIzaSyDE-edho0DTkfMbsGF9XoiOQgCPkVJInzU';
         // this.baseUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemma-3-27b-it:generateContent';
         // console.log('🤖 Gemini 키워드 추출기 생성됨 - Model: gemma-3-27b-it');
-        this.baseUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash-preview-image-generation:generateContent';
-        console.log('🤖 Gemini 키워드 추출기 생성됨 - Model: gemini-2.0-flash-preview-image-generation');
+        this.baseUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
+        console.log('🤖 Gemini 키워드 추출기 생성됨 - Model: gemini-2.0-flash');
     }
 
     /**
@@ -179,4 +179,5 @@ setTimeout(async () => {
     }
 
 }, 500);
+
 
